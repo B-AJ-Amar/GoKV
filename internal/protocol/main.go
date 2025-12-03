9e2cb16b1f55d0d447b5f168ca2ab52e9526a05c
Amar A.J Bouakaz <amarbouakaz91@gmail.com>
2025-12-04 00:56:50 +0100

    feat: define RESP interface and message types for protocol handling

--- internal/protocol/main.go
+++ internal/protocol/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+const (
+	simpleStrType   = '+'
+	SimpleErrorType = '-'
+	IntType         = ':'
+	BulkStrType     = '$'
+	ArrayType       = '*'
+	NullType        = '_'
+	BoolType        = '#'
+	DoubleType      = ','
+	BigNumType      = '('
+	BulkErrorType   = '!'
+	VerbatimStrType = '='
+	MapType         = '%'
+	SetType         = '~'
+	PushType        = '>'
+)
+
+type RESP interface {
+	Parse(msg string) RESPReq
+	Process(req *RESPReq) RESPRes
+}
+
 func main() {
 	fmt.Println("Hello protocol")
 }
